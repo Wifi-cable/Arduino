b79912aac155a502eec9a3f489d7d6286674def8
mary <Wifi-cable@users.noreply.github.com>
2016-07-12 12:03:32 +0200

    now to the additional blinky lights

--- teatimer/teatimer.ino
+++ teatimer/teatimer.ino
@@ -10,8 +10,27 @@ void setup(){
            pinMode(led8, OUTPUT);
            pinMode(led0, OUTPUT);
          }
-void loop(){ 
+void loop(){ // sollte blinken. aber blinken war nicht das ziel
   digitalWrite (led1, HIGH);
   delay (1000);
-  digitalWrite (led, LOW);
-  delay(1000);  
+  digitalWrite (led1, LOW);  //streichen wenn es funtzt
+  delay(1000); 
+  int J = 10;
+  int i = 0;
+  int k = 0;
+  
+  if (J==0){
+    digitalWrite(led0; HIGH);
+    delay (2000);
+  }
+    else{ 
+      if (j>=8){
+        digitalWrite(led8; HIGH)
+        delay(1000);
+     int i =j%2;
+     int k= j/2;// gut und jetzt die richtien leuchten...
+     }
+    }
+  }
+ 
+} 
