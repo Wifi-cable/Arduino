e98dbbb625c17e4aa9e4a96fc6278ce89b67af39
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-07-27 22:48:09 -0700

    tea_timer_compact.ino moved from tea_timer_array.ino
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

---
+++ tea_timer_compact/tea_timer_compact.ino
@@ -0,0 +1,71 @@
+
+#include <Servo.h>
+Servo myservo;
+int pos =0;
+//int i;
+//int bin[4];
+
+void setup(){ 
+  pinMode(2, OUTPUT); //led1
+  pinMode(3, OUTPUT); //led2
+  pinMode(4, OUTPUT); //led4
+  pinMode(5, OUTPUT); //led8
+  pinMode(6, OUTPUT); //led0
+  pinMode(9, OUTPUT);//grüner tester
+}
+
+void loop(){ 
+  schleife();
+} 
+void schleife(){
+   int mod;
+    int bin[]={2,3,4,5};
+     int j;
+       
+  for (j=10;j>=0; j--){ 
+      
+    int k=0;  
+    int i=j;
+    
+    while(i!=0) { 
+    mod= i%2;  //an welcher stelle muss das stehen?    ist gerade nie false, das ist ein problem, so gibts kein else...            
+   if (mod==1){  //ohne boolean versuchen?  if (i%2==1)
+  digitalWrite(bin[k], HIGH); 
+    k=k+1;
+    i=i/2;
+    //digitalWrite(9, HIGH);
+    
+    }
+  else { //else tritt nie ein. fehler im boolean
+    i=i/2;
+    k=k+1;
+    
+   /* digitalWrite(6, HIGH);
+    delay (200);
+    digitalWrite(6, LOW); */
+    
+  }
+   mod=0;  
+  //delay(500); 
+    }
+    
+      delay(1000);
+      digitalWrite(2, LOW);
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
+      //delay (900);
+      //digitalWrite(9,LOW);
+      delay(100);
+  }
+  delay(500);  // später wieder verlängern
+  digitalWrite(6, HIGH);
+  delay (1000);
+  digitalWrite(6, LOW);
+  delay(2000);
+
+  
+}
+
+
+
