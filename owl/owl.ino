3ff7797626cf60dc4fb5563fe723771e326b494c
mary <Wifi-cable@users.noreply.github.com>
2016-08-17 15:43:13 +0200

    function squeek. buzzer works annyingly loude

--- owl/owl.ino
+++ owl/owl.ino
@@ -4,6 +4,7 @@
   const int red = 4;
   const int yellow = 2;
   const int green = 3;
+  const int buzz=12;
   int serv=0;
  
 void setup (){ 
@@ -12,11 +13,16 @@ void setup (){
   pinMode(yellow, OUTPUT);
   pinMode(green, OUTPUT);
   eyes.attach(8);
+  pinMode(buzz, OUTPUT);//the buzzer or speaker
   //Serial.begin(300);  //rate of data transmission 
 }
 void loop(){ 
 
-   int sensorVal=analogRead(sensor); //light sensor 
+   //light();
+   squeek();
+}
+void light(){
+int sensorVal=analogRead(sensor); //light sensor 
    
   if (sensorVal>=801){ //500== fairly dark
   digitalWrite (red, HIGH);
@@ -38,9 +44,28 @@ else {
           delay(500);
         }
     }
-    //servotest
+   /* //servotest
     eyes.write(0);
     delay(200);
     eyes.write(20);
     delay(200);
+    */
+}
+void squeek(){
+/*note  frequency
+  c     262 Hz
+  d     294 Hz
+  e     330 Hz
+  f     349 Hz
+  g     392 Hz
+  a     440 Hz
+  b     494 Hz
+  C     523 Hz*/
+  int b;
+  //for (b=0; b>=10 ; b++){
+    tone(buzz, 262);
+    delay(500);
+    noTone(buzz);
+    delay(500);
+ // }
 }
