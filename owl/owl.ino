c9aa0690d74df2906b95039eef43220125815920
mary <Wifi-cable@users.noreply.github.com>
2016-08-20 11:43:05 +0200

    too random, commit anyhow to swithch branches

--- owl/owl.ino
+++ owl/owl.ino
@@ -1,13 +1,15 @@
-  #include<Servo.h>
-  Servo eyes;  // servo to move the owls eylids
-  const int sensor =A0;
-  const int red = 4;
-  const int yellow = 2;
-  const int green = 3;
-  const int buzz=12;
-  int serv=0;
-  //const int sensorVal=analogRead(sensor); //light sensor does not seem to work here
-  int sensorVal;
+#include<Servo.h>
+Servo eyes;  // servo to move the owls eylids
+const int sensor =A0;
+const int red = 4;
+const int yellow = 2;
+const int green = 3;
+const int buzz=12;
+int serv=0;
+//const int sensorVal=analogRead(sensor); //light sensor does not seem to work here
+int sensorVal;
+
+
 void setup (){ 
   pinMode(sensor, INPUT);
   pinMode (red, OUTPUT);
@@ -16,58 +18,59 @@ void setup (){
   eyes.attach(8);
   pinMode(buzz, OUTPUT);  //the buzzer or speaker
   Serial.begin(9600);  //rate of data transmission 
-  
+/* mach die Zeile oben mal zu 
+sensorVal= analogRead(sensor); // consistently global*/
 }
 void loop(){ 
- int sensorVal= analogRead(sensor); // local? 
-   //light();
-   //squeek();
-   light();
-   lightTest();
+ sensorVal= analogRead(sensor); // local? 
+  //light();
+  //squeek();
+  light();
+  lightTest(sensorVal); 
 }
 void light(){
-//int sensorVal=analogRead(sensor); //light sensor 
-   
+  //int sensorVal=analogRead(sensor); //light sensor 
+
   if (sensorVal>=801){ //500== fairly dark
-  digitalWrite (red, HIGH);
-  delay(500);
-  digitalWrite(red, LOW);
-  delay(500);
-    } 
-else { 
- if (sensorVal<=800 && sensorVal>=400){  // medium light
-  digitalWrite(yellow, HIGH);
-  delay(500);
-  digitalWrite(yellow, LOW);
-  delay(500);  
-      }
-      else {
-          digitalWrite(green, HIGH);
-          delay(500);
-          digitalWrite(green, LOW);
-          delay(500);
-        }
+    digitalWrite (red, HIGH);
+    delay(500);
+    digitalWrite(red, LOW);
+    delay(500);
+  } 
+  else { 
+    if (sensorVal<=800 && sensorVal>=400){  // medium light
+      digitalWrite(yellow, HIGH);
+      delay(500);
+      digitalWrite(yellow, LOW);
+      delay(500);  
     }
-   /* //servotest
-    eyes.write(0);
-    delay(200);
-    eyes.write(20);
-    delay(200);
-    */
+    else {
+      digitalWrite(green, HIGH);
+      delay(500);
+      digitalWrite(green, LOW);
+      delay(500);
+    }
+  }
+  /* //servotest
+   eyes.write(0);
+   delay(200);
+   eyes.write(20);
+   delay(200);
+   */
 }
 void squeek(){
-//int sensorVal=analogRead(sensor); //light sensor 
-/*note  frequency
-  c     262 Hz
-  d     294 Hz
-  e     330 Hz
-  f     349 Hz
-  g     392 Hz
-  a     440 Hz
-  b     494 Hz
-  C     523 Hz*/
- if (sensorVal<=800){
-  
+  //int sensorVal=analogRead(sensor); //light sensor 
+  /*note  frequency
+   c     262 Hz
+   d     294 Hz
+   e     330 Hz
+   f     349 Hz
+   g     392 Hz
+   a     440 Hz
+   b     494 Hz
+   C     523 Hz*/
+  if (sensorVal<=800){
+
     tone(buzz, 262);
     delay(300);
     noTone(buzz);
@@ -76,17 +79,20 @@ void squeek(){
     delay(100);
     noTone(buzz);
     delay(5000);
- }
- else {    //not reading the light sensor. why?
-   tone(buzz, 523);
-   delay (3000);
-   noTone(buzz);
-   delay(10000);
-     }
+  }
+  else {    //not reading the light sensor. why?
+    tone(buzz, 523);
+    delay (3000);
+    noTone(buzz);
+    delay(10000);
+  }
 }
-void sreech(){ }
-void lightTest(){
+void sreech(){ 
+}
+ void lightTest(){  // so nicht
   String test= "funst?"; //string to test the serial monitor, laptop is talking to serial
- Serial.println(sensorVal);  //read the level of light in my room
- Serial.println(test);
- }
+  Serial.println(sensorVal);  //read the level of light in my room
+  Serial.println(test);
+  //return sensorVal;
+}
+
