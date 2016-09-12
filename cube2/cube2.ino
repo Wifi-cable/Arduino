0f9b8fe651b198bd42609b9b13dfd56effc99ff3
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-09-12 11:08:55 -0700

    typo fix

--- cube2/cube2.ino
+++ cube2/cube2.ino
@@ -1,5 +1,5 @@
 //colums + 
-int LEDcol[]={0,1,2,3,4,5,6,7.8,9,10,11,12,13,A0,A1}; //digital outputs connected to positive or collums of LEDs
+int LEDcol[]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,A0,A1}; //digital outputs connected to positive or collums of LEDs
 int LEDlevel[]={A2,A3,A4,A5 };  //4 levels analoge
 int pin,  //output pin for colum in LEDcol array
     fl, //flicker to multiplex
