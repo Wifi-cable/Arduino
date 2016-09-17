ecf002bc169e59d194ab57719b35686aede9f3c4
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-09-17 16:19:40 -0700

    cube_simulator.ino : all on
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

---
+++ cube_simulator/cube_simulator.ino
@@ -0,0 +1,22 @@
+//colums + 
+int LEDcol[]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,A0,A1}; //digital outputs connected to positive or collums of LEDs
+int LEDlevel[]={A2,A3,A4,A5 };  //4 levels analog
+
+void setup()
+{
+  for (int anode= 0; anode<16; anode++)
+  {
+    pinMode(LEDcol[anode], OUTPUT);
+    digitalWrite(LEDcol[anode],HIGH);
+  }
+  for (int cathode=0; cathode<4; cathode++)
+  {
+    pinMode(LEDlevel[cathode], OUTPUT);
+    digitalWrite(LEDlevel[cathode],LOW);
+  }
+}
+
+void loop()
+{
+  
+}
