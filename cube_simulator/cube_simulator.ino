f9ee6de7eb019198e0645e2b86b2839ebf6a9e29
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-09-17 17:52:42 -0700

    cube_simulator.ino : quick & dirty hardware test
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

--- cube_simulator/cube_simulator.ino
+++ cube_simulator/cube_simulator.ino
@@ -2,11 +2,29 @@
 int LEDcol[]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,A0,A1}; //digital outputs connected to positive or collums of LEDs
 int LEDlevel[]={A2,A3,A4,A5 };  //4 levels analog
 
+int levels= 0;
+
+void off()
+{
+  for (int anode= 0; anode<16; pinMode(LEDcol[anode++],INPUT));
+  for (int cathode= 0; cathode<4; pinMode(LEDlevel[cathode++],INPUT));
+}
+
 void planeBinary(int n)
 {
   for (int b=0; b<16; b++)
   {
     digitalWrite(LEDcol[b],n&1);
+    n>>=1;
+  }
+}
+
+void levelBinary(int n)
+{
+  n= ~n;
+  for (int l=0; l<4; l++)
+  {
+    digitalWrite(LEDlevel[l],1&n);
     n>>=1;
   }
 }
@@ -27,40 +45,11 @@ void setup()
 
 void loop()
 {
+  levelBinary(levels++);
+  levels &=15;
   for (int n=0; n<256; n++)
   {
-    planeBinary(n);
+    planeBinary(n|n<<8);
     delay(50);
   }
-  for (int cathode=0; cathode<4; digitalWrite(LEDlevel[cathode++],HIGH));
-  for (int n=0; n<256; n++)
-  {
-    planeBinary(n);
-    delay(50);
-  }
-  for (int cathode=0; cathode<4; digitalWrite(LEDlevel[cathode++],LOW));
-  for (int n=0; n<256; n++)
-  {
-    planeBinary(n);
-    delay(50);
-  }
-  for (int cathode=0; cathode<4; pinMode(LEDlevel[cathode++],INPUT));
-  for (int n=0; n<256; n++)
-  {
-    planeBinary(n);
-    delay(50);
-  }
-  for (int cathode=0; cathode<4; digitalWrite(LEDlevel[cathode++],HIGH));
-  for (int n=0; n<256; n++)
-  {
-    planeBinary(n);
-    delay(50);
-  }
-  for (int cathode=0; cathode<4; digitalWrite(LEDlevel[cathode++],LOW));
-  for (int n=0; n<256; n++)
-  {
-    planeBinary(n);
-    delay(50);
-  }
-  for (int cathode=0; cathode<4; pinMode(LEDlevel[cathode++],OUTPUT));
 }
