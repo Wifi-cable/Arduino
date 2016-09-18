ae8b793d3b7e9d0e4c5f7ddfd378e9374f9af596
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-09-17 21:30:41 -0700

    servo_test.ino : move servo smoothly
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

--- servo_test/servo_test.ino
+++ servo_test/servo_test.ino
@@ -1,20 +1,23 @@
 #include <Servo.h>
 Servo myservo;
-int pos_oben = 0;
-int pos_unten = 180;
+const int pos_oben = 90;
+const int pos_unten = 0;
 
-int toggle;
+int pos;
+int dir;
 
 void setup()
 {
   myservo.attach(7);
-  toggle= 0;
-  myservo.write(90);
+  dir= 0;
+  pos= 90;
+  myservo.write(pos);
 }
 
 void loop()
 {
-  myservo.write(toggle? pos_unten : pos_oben);
-  toggle= ~toggle;
-  delay(500);
+  myservo.write(pos);
+  pos+= dir? -1:1;
+  if (pos_oben < pos || pos_unten >pos) dir= !dir;
+  delay(10);
 }
