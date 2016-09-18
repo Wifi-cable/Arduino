b3148f2cf0b3160126da5ae155caeaf5e58fba5b
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-09-17 18:21:42 -0700

    cube_simulator.ino : bitreversed blinking
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

--- cube_simulator/cube_simulator.ino
+++ cube_simulator/cube_simulator.ino
@@ -2,12 +2,24 @@
 int LEDcol[]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,A0,A1}; //digital outputs connected to positive or collums of LEDs
 int LEDlevel[]={A2,A3,A4,A5 };  //4 levels analog
 
-int levels= 0;
+int level= 0;
 
 void off()
 {
   for (int anode= 0; anode<16; pinMode(LEDcol[anode++],INPUT));
   for (int cathode= 0; cathode<4; pinMode(LEDlevel[cathode++],INPUT));
+}
+
+int bitreverse(int n)
+{
+  int r= 0;
+  for (int b=0; b<16; b++)
+  {
+    r<<=1;
+    r|= n&1;
+    n>>=1;
+  }
+  return r;
 }
 
 void planeBinary(int n)
@@ -17,6 +29,12 @@ void planeBinary(int n)
     digitalWrite(LEDcol[b],n&1);
     n>>=1;
   }
+}
+
+void planeReverse(int n)
+{
+  for (int b=16; b>0; digitalWrite(LEDcol[--b],n&1))
+    n>>=1;
 }
 
 void levelBinary(int n)
@@ -41,15 +59,14 @@ void setup()
     pinMode(LEDlevel[cathode], OUTPUT);
     digitalWrite(LEDlevel[cathode],LOW);
   }
+  delay(1000);
 }
 
 void loop()
 {
-  levelBinary(levels++);
-  levels &=15;
   for (int n=0; n<256; n++)
   {
-    planeBinary(n|n<<8);
+    planeBinary(n|bitreverse(n));
     delay(50);
   }
 }
